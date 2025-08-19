cfde0c2c96e54614f500e5b20adbf306c9a14102
TeamWave-Official <teamwave-hq@outlook.com>
2025-08-19 12:08:50 +0530

    Add files via upload
    Introduced Wave Bash v0.0.1 (alpha)

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace WaveBash
+{
+    public partial class App : Application
+    {
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            MainWindow window = new MainWindow();
+            window.Activate();
+        }
+    }
+}
